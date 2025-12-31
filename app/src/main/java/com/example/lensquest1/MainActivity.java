83fe4814ba45058ac7abe98697c61c08898850a1
RoyalDog3 <Itaydagan2007@gmail.com>
2025-12-31 15:28:54 +0200

    sign style and homepage

--- app/src/main/java/com/example/lensquest1/MainActivity.java
+++ app/src/main/java/com/example/lensquest1/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.lensquest1;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+/**
+ * This is the main activity of the application.
+ * It is displayed after the welcome screen.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * This method is called when the activity is first created.
+     * It is used to initialize the activity, such as setting the layout.
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Calls the superclass's onCreate method to complete the creation of the activity
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Sets the layout for this activity
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
---
+++ app/src/main/java/com/example/lensquest1/Sign.java
@@ -0,0 +1,16 @@
+package com.example.lensquest1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Connects this activity to its XML layout file
+        setContentView(R.layout.activity_sign);
+    }
+}
